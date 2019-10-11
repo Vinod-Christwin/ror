f9854f75cea11b5653101f69766844da455c1e2f
Vinod <christwinpr@gmail.com>
2019-10-11 17:28:07 +0530

    Added database connection between projects and tasks

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,7 @@
 class Project < ApplicationRecord
+  has_many :tasks
   after_initialize :set_defaults
+  validates_presence_of :title, :description, :percent_complete
   scope :almost_completed, -> { where('percent_complete > 75.0')}
 
   def set_defaults
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_04_091731) do
+ActiveRecord::Schema.define(version: 2019_10_11_115518) do
 
   create_table "projects", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,16 @@ ActiveRecord::Schema.define(version: 2019_10_04_091731) do
     t.decimal "percent_complete"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "tasks", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.integer "project_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.boolean "completed"
+    t.index ["project_id"], name: "index_tasks_on_project_id"
   end
 
 end
